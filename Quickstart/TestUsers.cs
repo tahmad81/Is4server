bf185fd9915ce3500c3bd8673096517e3f22219f
Tauseef <tauseef-ahmad@hotmail.com>
2021-09-22 00:07:52 +0500

    Add project files.

---
+++ Quickstart/TestUsers.cs
@@ -0,0 +1,57 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "2",
+                        Username = "admin",
+                        Password = "admin",
+                        Claims =
+                        {
+                            
+                            new Claim(JwtClaimTypes.Role,"admin")
+
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "2",
+                        Username = "admin",
+                        Password = "admin",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Role,"admin")
+
+            }
+                    }
+                };
+            }
+        }
+    }
+}
